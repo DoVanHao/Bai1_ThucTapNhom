3589064c73dac99a78f8ba9b540a0d8cd7b1006a
MinhHieu <rezo.krifferson@gmail.com>
2019-09-13 21:22:24 +0700

    Update DangKy.cs

--- BTL_QLNS/BTL_QLNS/DangKy.cs
+++ BTL_QLNS/BTL_QLNS/DangKy.cs
@@ -68,7 +68,39 @@ namespace BTL_QLNS
             frmdn.Show();
             this.Hide();
         }
+        private void btnDangnhap_Click(object sender, EventArgs e)
+        {
+            DangNhap frmdn = new DangNhap();
+            frmdn.Show();
+            this.Hide();
+        }
 
+        private void btnDangky_Click(object sender, EventArgs e)
+        {
+            User_BUS ub = new User_BUS();
+            try
+            {
+                if (txtNhaplai.Text == txtMatkhau.Text)
+                {
+                    ub.insertUser(txtTaikhoan.Text, txtMatkhau.Text, txtMaNv.Text);
+                    MessageBox.Show("Đăng ký tài khoản thành công !");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Mật khẩu nhập lại không đúng !");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Bạn nhập sai cú pháp !");
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Lỗi kết nối CSDL!");
+            }
+
+        }
         private void btnDangky_Click_double(object sender, EventArgs e)
         {
             User_BUS ub = new User_BUS();
