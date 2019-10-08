b18c0936f9c67355606f03e9911a43dce811ab86
Nam <namnguyen.lion@gmail.com>
2019-10-08 20:17:19 +0700

    Update User_BUS.cs

--- BTL_QLNS/BTL_QLNS/User_BUS.cs
+++ BTL_QLNS/BTL_QLNS/User_BUS.cs
@@ -24,5 +24,24 @@ namespace BTL_QLNS.BUS
             String sql = "insert into DANGNHAP values('" + username + "','" + pass + "','" + manv + "')";
             da.ExcuteNonQuery(sql);
         }
+		        private void btnThemSP_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dgvSPX.Rows.Count; i++)
+            {
+                if (dgvSPX.Rows[i].Cells[0].Value == dgvSP.Rows[dgvSP.SelectedRows[0].Index].Cells[0].Value)
+                {
+                    MessageBox.Show("Sản phẩm đã được chọn !!!", "Nhắc nhở", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+            if (dgvSP.SelectedRows.Count > 0)
+            {
+                dgvSPX.Rows.AddRange(new DataGridViewRow());
+                dgvSPX.Rows[dgvSPX.RowCount - 2].Cells[0].Value = dgvSP.Rows[dgvSP.SelectedRows[0].Index].Cells[0].Value;
+                dgvSPX.Rows[dgvSPX.RowCount - 2].Cells[1].Value = numericUpDownSL.Value;
+                dgvSPX.Rows[dgvSPX.RowCount - 2].Cells[2].Value = numericUpDownGN.Value;
+                dgvSPX.Rows[dgvSPX.RowCount - 2].Cells[3].Value = int.Parse(numericUpDownSL.Value.ToString()) * long.Parse(numericUpDownGN.Value.ToString());
+            }
+        }
     }
 }
