5bc3905ddf5bb069a06e8be734499f55dfa18420
pencil003 <56083400+pencil003@users.noreply.github.com>
2019-10-10 10:06:55 +0700

    Update DangKy.cs

--- BTL_QLNS/BTL_QLNS/DangKy.cs
+++ BTL_QLNS/BTL_QLNS/DangKy.cs
@@ -127,5 +127,38 @@ namespace BTL_QLNS
             }
 
         }
+
+private void btnDangky_Click(object sender, EventArgs e)
+        {
+            User_BUS ub = new User_BUS();
+            try
+            {
+                if (txtNhaplai.Text == txtMatkhau.Text)
+                {
+                    ub.insertUser(txtTaikhoan.Text, txtMatkhau.Text, txtMaNv.Text);
+                    MessageBox.Show("Đăng ký tài khoản thành công !");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Mật khẩu nhập lại không đúng !");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Bạn nhập sai cú pháp !");
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Lỗi kết nối CSDL!");
+            }
+
+        }
+		
+		// them
+		private void DangKy_Loadform(object sender, EventArgs e)
+        {
+
+        }
     }
 }
