b5a5ab9d371d6ab2094c4062a98c00d64652f9ea
MinhHieu <rezo.krifferson@gmail.com>
2019-10-20 21:27:15 +0700

    quan li du an

--- BTL_QLNS/BTL_QLNS/Quanlyduan.cs
+++ BTL_QLNS/BTL_QLNS/Quanlyduan.cs
@@ -190,6 +190,33 @@ private void dgvNhanVien_CellClick(object sender, DataGridViewCellEventArgs e)
             txtSearch.Text = "";
             txtTenNv.Text = "";
         }
+		
+		        private void btnDangky_Click(object sender, EventArgs e)
+        {
+            User_BUS ub = new User_BUS();
+            try
+            {
+                if (txtNhaplai.Text == txtMatkhau.Text)
+                {
+                    ub.insertUser(txtTaikhoan.Text, txtMatkhau.Text, txtMaNv.Text);
+                    MessageBox.Show("Đăng ký tài khoản thành công !");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Mật khẩu nhập lại không đúng !");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Bạn nhập sai cú pháp !");
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Lỗi kết nối CSDL!");
+            }
+
+        }
 
         private void ExportExcel(DataGridView dgv, string duongDan, string tenTap)
         {
